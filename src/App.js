4cc0f14bb3bac0e00a1e192790acde85238033b6
Sanjay <sanjaylachat@gmail.com>
2018-01-02 20:20:20 +0530

    First Run

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: ""
+    };
+  }
 
-class App extends Component {
+  componentDidMount() {
+    this.setState({ test: "Sanjay" });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div class="MyApp">Test Messsage : {this.state.test}</div>;
   }
 }
-
-export default App;
