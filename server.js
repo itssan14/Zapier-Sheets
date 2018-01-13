28371a733058b8d7712b6b276f1ed522f437aef6
Sanjay <sanjaylachat@gmail.com>
2018-01-13 15:54:26 +0530

    Conflict solved running

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Post Request
 app.post("/info", (req, res) => {
-<<<<<<< HEAD
-  res.send(
-    `Entered data was succesfully added into the table ${req.body.tableName}`
-  );
-=======
-  res.send(`Entered message was succesfully sent`);
->>>>>>> 3d158ff679d14adf0f5e6b61da74063caf6d62ca
+  res.send(`Entered data was succesfully added into the table ${req.body.tableName}`);
   console.log(req.body);
 });
 //Listening for request
