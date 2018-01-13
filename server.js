3d158ff679d14adf0f5e6b61da74063caf6d62ca
Sanjay <sanjaylachat@gmail.com>
2018-01-13 15:29:42 +0530

    New fields introduced && JSON object available in console

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//Post Request
 app.post("/info", (req, res) => {
-  res.send("Succesfully Recieved");
+  res.send(`Entered message was succesfully sent`);
+  console.log(req.body);
 });
-
+//Listening for request
 const port = 3001 || process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running at PORT : ${port}`);
