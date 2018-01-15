cd8c064e814c2f69e21a5e838668d95254e494f0
Sanjay S <sanjaylachat@gmail.com>
2018-01-15 10:34:28 +0530

    Switched back to yarn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import "normalize-css";
 import NotFound from "./components/NotFound";
 import App from "./App";
-import Login from "./components/LoginForm";
+import Login from "./components/LoginPage";
 import registerServiceWorker from "./registerServiceWorker";
 
 const Router = () => (
